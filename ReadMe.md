ff3b0c7d3eb5a9f998c0a60552e53f9b763820e0
Yumei <yumei@YMacBook-Pro.local>
2021-03-11 14:53:56 -0500

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+Following tutorial: 
+https://dev.to/vladyslavnua/how-to-build-a-django-web-app-from-scratch-tutorial-5bp0
---
+++ cooking/urls.py
@@ -0,0 +1,16 @@
+"""cooking URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('u/', include('users.urls')),
+    path('markdownx/', include('markdownx.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
